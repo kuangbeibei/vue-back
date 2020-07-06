8f60eb2c804f1aa8a1990da55b47f09ecbde665c
kup <332518418@qq.com>
2020-07-06 14:17:11 +0800

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+let users = [{
+    id: 1,
+    name: 'zhufeng1'
+}, {
+    id: 2,
+    name: 'zhufeng2'
+}, {
+    id: 3,
+    name: 'zhufeng3'
+}]
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.url === '/api/users') {
+        res.end(JSON.stringify(users))
+    } else {
+        res.end('NOT FOUND API')
+    }
+});
+server.listen(3000, () => {
+    console.log('后端接口服务期已经启动在3000端口')
+});
